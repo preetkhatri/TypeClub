a3bdfea8ced01f04e07c5e9e654089120a24d4b2
preetkhatri <preetkhatri07@gmail.com>
2024-07-04 22:11:13 +0530

    added the cursor and allwoed user to select the word length

--- script.js
+++ script.js
@@ -1,119 +1,119 @@
-// vocabulary
+let words = [];
 
-let words = [
-    "have", "is", "our", "of", "what", "all", "by", "say", "her", "well",
-    "out", "us", "friend", "the", "any", "new", "those", "want", "see", "much",
-    "men", "should", "up", "in", "world", "go", "about", "only", "thought", "people",
-    "could", "other", "than", "its", "work", "also", "play", "small", "state", "even",
-    "need", "change", "beautiful", "form", "found", "happen", "risk", "full", "culture", "year",
-    "day", "life", "school", "famliy", "house", "present", "point", "service", "government",
-    "company", "money", "information", "first", "america", "make", "second", "perspective",
-    "and", "thought", "business", "literature", "development", "public", "health", "law",
-    "compare", "provide", "system", "particularly", "potential", "society", "economic",
-    "interest", "relationships", "education", "article", "importance", "environment",
-    "technology", "industry", "healthcare", "available", "environment", "because", "analysis",
-    "investment", "policy", "behavior", "particular", "necessary", "opportunity", "international",
-    "treatment", "research", "customer", "report", "development", "marketing", "purchase",
-    "agreement", "discussion", "advantage", "relationship", "production", "responsibility",
-    "experience", "technology", "management", "organization", "financial", "information",
-    "market", "employee", "analysis", "discussion", "goal", "support", "advantage",
-    "creative", "interest", "strategy", "decision", "resource", "customer", "service", "delivery",
-    "performance", "marketing", "sale", "technology", "innovation", "partner", "project", "management",
-    "leader", "team", "skill", "motivation", "culture", "environment", "communication", "problem",
-    "solving", "planning", "goal", "setting", "evaluation", "feedback", "time", "management", "pressure",
-    "stress", "conflict", "resolution", "change", "management", "diversity", "inclusion", "ethics",
-    "corporate", "social", "responsibility", "globalization", "sustainability", "economics", "finance",
-    "accounting", "marketing", "human", "resources", "management", "information", "technology", "operations",
-    "research", "and", "development", "engineering", "sales", "legal", "compliance", "risk", "management",
-    "audit", "internal", "control", "tax", "finance", "budget", "forecast", "cash", "flow", "investment",
-    "banking", "insurance", "real", "estate", "manufacturing", "construction", "transportation",
-    "communication", "utilities", "wholesale", "retail", "trade", "services", "public", "sector",
-    "non", "profit", "organization", "entrepreneur", "startup", "business", "plan", "marketing",
-    "strategy", "sales", "force", "management", "customer", "relationship", "management", "advertising",
-    "public", "relations", "social", "media", "branding", "communication", "negotiation", "leadership",
-    "teamwork", "motivation", "problem", "solving", "decision", "making", "critical", "thinking",
-    "creativity", "innovation", "time", "management", "stress", "management", "conflict", "resolution",
-    "ethics", "integrity", "corporate", "social", "responsibility", "globalization", "sustainability",
-    "economics", "microeconomics", "macroeconomics", "supply", "demand"];
+// API call
 
-const len = words.length;
+async function apiCall(wordLength) {
+    const apiUrl = `https://random-word-api.herokuapp.com/word?number=100&length=${wordLength}`;
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error("Network response was not OK!");
+        }
+        const data = await response.json();
+        words = [...words, ...data]; 
+    } catch (error) {
+        console.log("Error is", error);
+    }
+}
+
 const typeTime = 30000;
 window.timer = null;
 window.start = null;
+let isGameOver = false;
 
 // Generating Random words
-
 function randomWord() {
-    const idx = Math.floor(Math.random() * len);
+    const idx = Math.floor(Math.random() * words.length);
     return words[idx];
 }
 
+// Closing modal
+function closeModal() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal').style.display = 'none';
+}
+
+document.getElementById('saveWordLengthBtn').addEventListener('click', async function () {
+    var wordLength = document.getElementById('wordLengthSelect').value;
+    console.log('Selected word length:', wordLength);
+    closeModal();
+    await apiCall(wordLength);
+    console.log(words);
+    newGame();
+});
+
 // Addition & removal of class
-
-function addClass(e,name) {
-    e.className += ' '+name;
+function addClass(e, name) {
+    e.className += ' ' + name;
 }
 
-function removeClass(e,name) {
-    e.className = e.className.replace(name,'');
+function removeClass(e, name) {
+    e.className = e.className.replace(name, '');
 }
 
-// converting letters into a span format
-
+// Converting letters into a span format
 function format(word) {
     return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`
 }
 
-// new test begin
-
+// New test begin
 function newGame() {
+    console.log("new game words are", words);
     document.getElementById('words').innerHTML = '';
     for (let i = 0; i < 100; i++) {
         document.getElementById('words').innerHTML += format(randomWord());
     }
     addClass(document.querySelector('.word'), 'current');
     addClass(document.querySelector('.letter'), 'current');
-    document.getElementById("timer").innerHTML = typeTime/1000;
+    document.getElementById("timer").innerHTML = typeTime / 1000;
     window.timer = null;
 }
 
-// listening key press
+// Function to update the cursor's position
+function updateCursor() {
+    const currentWord = document.querySelector('.word.current');
+    const currentLetter = document.querySelector('.letter.current');
+    const cursor = document.getElementById('cursor');
+    if (currentLetter) {
+        const rect = currentLetter.getBoundingClientRect();
+        const gameRect = document.getElementById('game').getBoundingClientRect();
+        cursor.style.left = `${rect.left - gameRect.left}px`;
+        cursor.style.top = `${rect.top - gameRect.top}px`;
+    } else if (currentWord) {
+        // Move cursor to the end of the current word if no current letter is found
+        const lastLetter = currentWord.lastChild;
+        if (lastLetter) {
+            const rect = lastLetter.getBoundingClientRect();
+            const gameRect = document.getElementById('game').getBoundingClientRect();
+            cursor.style.left = `${rect.right - gameRect.left}px`;
+            cursor.style.top = `${rect.top - gameRect.top}px`;
+        }
+    }
+}
 
-document.getElementById('game').addEventListener('keydown',(e)=> {
+// Listening to key press and updating the cursor position
+document.getElementById('game').addEventListener('keydown', (e) => {
+    if (isGameOver) return;
     const key = e.key;
     const currentWord = document.querySelector('.word.current');
     const currentLetter = document.querySelector('.letter.current');
-    let expected = currentLetter;
-    if(expected===null) expected=' ';
-    else expected=currentLetter.innerHTML;
-    const isLetter = key.length===1 && key!==' ';
-    const isSpace = key===' ';
-    const isBackspace = key==="Backspace";
-    const isFirstLetter = currentLetter===currentWord.firstChild;
+    let expected = currentLetter ? currentLetter.innerHTML : ' ';
+    const isLetter = key.length === 1 && key !== ' ';
+    const isSpace = key === ' ';
+    const isBackspace = key === "Backspace";
+    const isFirstLetter = currentLetter === currentWord.firstChild;
 
-    if(e.keyCode === 9) {
-        location.reload();
-    }
-
-    // checking if time is up then it returns
-
-    if(document.querySelector('#game.over')) {
-        return;
-    }
-
-    // for timing record
-
-    if(!window.timer) {
+    // Handling time check and starting timer if not already started
+    if (!window.timer) {
         window.timer = setInterval(() => {
-            if(!window.start) {
+            if (!window.start) {
                 window.start = (new Date()).getTime();
             }
             const currentTime = (new Date()).getTime();
             const msPassed = currentTime - window.start;
-            const sPassed = Math.round(msPassed/1000);
-            const sLeft = (typeTime/1000)-sPassed;
-            if(sLeft<=0) {
-                // document.getElementById("timer").innerHTML = sLeft;
+            const sPassed = Math.round(msPassed / 1000);
+            const sLeft = (typeTime / 1000) - sPassed;
+            if (sLeft <= 0) {
                 gameOver();
                 return;
             }
@@ -121,108 +121,90 @@ document.getElementById('game').addEventListener('keydown',(e)=> {
         }, 1000);
     }
 
-    // checking if we are on a letter
-
-    if(isLetter) {
-        if(currentLetter) {
-            // depending upon the typed keyword (correct or not), a class is assigned
-            let classType;
-            if(key===expected) classType='correct';
-            else classType='incorrect';
-            addClass(currentLetter,classType);
-            removeClass(currentLetter,'current');
-            if(currentLetter.nextSibling) {
-                addClass(currentLetter.nextSibling,'current');
+    // Handling letter typing
+    if (isLetter) {
+        if (currentLetter) {
+            let classType = key === expected ? 'correct' : 'incorrect';
+            currentLetter.classList.add(classType);
+            currentLetter.classList.remove('current');
+            if (currentLetter.nextSibling) {
+                currentLetter.nextSibling.classList.add('current');
             }
         } else {
             const appendWords = document.createElement('span');
             appendWords.innerHTML = key;
             appendWords.className = 'letter incorrect extra';
-            // currentWord.appendChild(appendWords);
         }
     }
 
-    // handling space type
-
-    if(isSpace) {
-        if(expected!==' ') {
-            // if typed space instead of an expected word
-            const letterSkip = [...document.querySelectorAll('.word.current .letter:not(.correct)')]
-            letterSkip.forEach((e)=>{
-                addClass(e,'incorrect');
-            })
+    // Handling space typing
+    if (isSpace) {
+        if (expected !== ' ') {
+            const letterSkip = [...document.querySelectorAll('.word.current .letter:not(.correct)')];
+            letterSkip.forEach((e) => e.classList.add('incorrect'));
         }
-        removeClass(currentWord,'current');
-        addClass(currentWord.nextSibling,'current');
-
-        if(currentLetter) removeClass(currentLetter,'current')
-        addClass(currentWord.nextSibling.firstChild,'current')
+        currentWord.classList.remove('current');
+        currentWord.nextSibling.classList.add('current');
+        if (currentLetter) currentLetter.classList.remove('current');
+        currentWord.nextSibling.firstChild.classList.add('current');
     }
 
-    // handling backspace
-
-    if(isBackspace) {
-        // on the first letter of the word
-        if(currentLetter && isFirstLetter) {
-            removeClass(currentWord,'current');
-            addClass(currentWord.previousSibling,'current');
-            removeClass(currentLetter,'current');
-            addClass(currentWord.previousSibling.lastChild,'current');
-            removeClass(currentWord.previousSibling.lastChild,'incorrect');
-            removeClass(currentWord.previousSibling.lastChild,'correct');
+    // Handling backspace
+    if (isBackspace) {
+        if (currentLetter && isFirstLetter) {
+            currentWord.classList.remove('current');
+            currentWord.previousSibling.classList.add('current');
+            currentLetter.classList.remove('current');
+            currentWord.previousSibling.lastChild.classList.add('current');
+            currentWord.previousSibling.lastChild.classList.remove('incorrect', 'correct');
         }
-        // not the first letter
-        if(currentLetter && !isFirstLetter) {
-            removeClass(currentLetter,'current');
-            addClass(currentLetter.previousSibling,'current');
-            removeClass(currentLetter.previousSibling, 'correct');
-            removeClass(currentLetter.previousSibling, 'incorrect');
+        if (currentLetter && !isFirstLetter) {
+            currentLetter.classList.remove('current');
+            currentLetter.previousSibling.classList.add('current');
+            currentLetter.previousSibling.classList.remove('correct', 'incorrect');
         }
-        if(!currentLetter) {
-            addClass(currentWord.lastChild,'current');
-            removeClass(currentWord.lastChild,'correct');
-            removeClass(currentWord.lastChild,'incorrect');
+        if (!currentLetter) {
+            currentWord.lastChild.classList.add('current');
+            currentWord.lastChild.classList.remove('correct', 'incorrect');
         }
     }
 
-    // for scrolling down of the lines and showing bottom lines
-    if(currentWord.getBoundingClientRect().top>220) {
+    // Scrolling down lines
+    if (currentWord.getBoundingClientRect().top > 220) {
         const words = document.getElementById('words');
         const margin = parseInt(words.style.marginTop || '0px');
         words.style.marginTop = (margin - 35) + 'px';
     }
 
-})
+    updateCursor();
+});
 
-// calculation of wpm
-
+// Calculation of wpm
 function result() {
     const allWords = [...document.querySelectorAll('.word')];
     const lastTyped = document.querySelector('.word.current');
     const lastTypedIdx = allWords.indexOf(lastTyped);
-    const typedWords = allWords.slice(0,lastTypedIdx);
-    const correctWords = typedWords.filter((e)=>{
+    const typedWords = allWords.slice(0, lastTypedIdx);
+    const correctWords = typedWords.filter((e) => {
         const letters = [...e.children];
-        const incorrectLetters = letters.filter(k=>k.className.includes('incorrect'))
-        const correctLetters = letters.filter(k=>k.className.includes('correct'));
-        return incorrectLetters.length===0 && correctLetters.length === letters.length;
+        const incorrectLetters = letters.filter(k => k.className.includes('incorrect'));
+        const correctLetters = letters.filter(k => k.className.includes('correct'));
+        return incorrectLetters.length === 0 && correctLetters.length === letters.length;
     });
 
     console.log(correctWords);
 
-    return (correctWords.length/typeTime)*60000;
+    return (correctWords.length / (typeTime / 60000));
 }
 
-// when game is over then simply add the class & show the wpm 
-
+// When game is over then simply add the class & show the wpm 
 function gameOver() {
+    isGameOver = true;
     clearInterval(window.timer);
-    addClass(document.getElementById('game'),'over');
-    document.getElementById("timer").innerHTML = `WPM: ${result()}`
+    addClass(document.getElementById('game'), 'over');
+    document.getElementById("timer").innerHTML = `WPM: ${result()}`;
 }
 
-document.getElementById("fight").addEventListener("click", ()=>{
+document.getElementById("fight").addEventListener("click", () => {
     location.reload();
-})
-
-newGame();
+});
